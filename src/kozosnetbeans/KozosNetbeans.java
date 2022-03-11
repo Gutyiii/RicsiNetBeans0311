3d75a728689e0b1f14716c29d512eba389d36043
Ricsi0901 <78799831+Ricsi0901@users.noreply.github.com>
2022-03-11 15:11:17 +0100

    nev 3 létrehozása

--- src/kozosnetbeans/KozosNetbeans.java
+++ src/kozosnetbeans/KozosNetbeans.java
@@ -6,7 +6,9 @@ public class KozosNetbeans {
 
     
     public static void main(String[] args) {
-        String nev1,nev2;
+        String nev1;
+        String nev2;
+        String nev3;
     }
     
 }
